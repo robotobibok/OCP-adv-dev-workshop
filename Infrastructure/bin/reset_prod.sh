5d87ab433ef0f6a3e5ac03ece4ff462f1115571b
ESKOM IT <mateusz.buczel@eskom.eu>
2018-10-02 12:48:35 +0200

    reset prod

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -18,3 +18,20 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
+# Delete the mlbparks blue service
+oc delete svc mlbparks-blue -n ${GUID}-parks-prod
+
+# Change MLBparks route
+oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-green"}}}' -n $GUID-parks-prod || echo "MLBParks already green"
+
+# Delete the nationalparks blue service
+oc delete svc nationalparks-blue -n ${GUID}-parks-prod
+
+# Change nationalparks route
+oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-green"}}}' -n $GUID-parks-prod || echo "NationalParks already green"
+
+# Delete the parksmap blue service
+oc delete svc parksmap-blue -n ${GUID}-parks-prod
+
+# Switch parksmap frontend to green
+oc patch route/parksmap -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n $GUID-parks-prod || echo "ParksMap already green"
